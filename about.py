10f09ea980faacc1d2f5bc1a7a64a823e3b04b43
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-11 16:02:25 +0530

    Update about.py

--- about.py
+++ about.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 def about():
     st.markdown(
-    """
-    <div class="content-container2">
-        <h1 class="custom-heading">About Me</h1>
-    </div>
+    """  
+    <h1 class="custom-heading">About Me</h1>
     <div>
         <h3 class="custom-lines2"><br>Aspiring AI/ML Engineer 💻🧠 &nbsp; | &nbsp; Python Developer 🐍 &nbsp; | &nbsp; Full Stack Python Developer 🕸️</h3>
         <h1 class="custom-heading2">EDUCATION</h1>
