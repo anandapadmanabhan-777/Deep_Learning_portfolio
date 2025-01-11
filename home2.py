4c9acb51d7a46bfce44e73d936d2278ea08aac69
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-11 14:24:51 +0530

    Update home2.py

--- home2.py
+++ home2.py
@@ -1,3 +1,40 @@
+#END
 import streamlit as st
+
 def home2():
-  st.write("Hiii")
+    st.markdown(
+    """
+    <div class="content-container2">
+        <h1 class="custom-heading">Welcome to My AI Portfolio..!!</h1>
+    </div>
+    <br>
+    <h1 class= "custom-heading2">Hello, I'm<br> ANANDAPADMANABHAN..!!</h1><br>
+    <div class="content-wrapper">
+        <h3 class="custom-lines">Python Developer | Aspiring AI/ML Engineer | Full Stack Python Developer &nbsp; 💻🧠<br><br>       
+    </div>
+    <br>
+    <div class="social-icons" style="display: flex; justify-content: center; gap: 20px; margin-top: 20px;">
+        <div>
+            <a href="https://www.linkedin.com/in/anandapadmanabhan-s-43959522a/" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" alt="LinkedIn Profile" width="50">
+            </a>
+            <p>LinkedIn</p>
+        </div>
+        <div>
+            <a href="mailto:anandapadmanabhan777@gmail.com">
+                <img src="https://cdn-icons-png.flaticon.com/512/552/552486.png" alt="Email Me" width="50">
+            </a>
+            <p>Email</p>
+        </div>
+        <div>
+            <a href="https://github.com/anandapadmanabhan-777" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" alt="GitHub Profile" width="50">
+            </a>
+            <p>GitHub</p>
+        </div>
+    </div>
+    <br>
+    
+    """,
+    unsafe_allow_html=True,    
+)
