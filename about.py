dd6cb7df2a0f51015c71a53d07f0a047d48f0f0c
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-11 16:06:00 +0530

    Update about.py

--- about.py
+++ about.py
@@ -6,6 +6,7 @@ def about():
     <h1 class="custom-heading">About Me</h1>
     <div>
         <h3 class="custom-lines2"><br>Aspiring AI/ML Engineer 💻🧠 &nbsp; | &nbsp; Python Developer 🐍 &nbsp; | &nbsp; Full Stack Python Developer 🕸️</h3>
+        <br>
         <h1 class="custom-heading2">EDUCATION</h1>
         <h3 class="custom-lines3"> &nbsp; ● BTech in Infomation Technology<br> &nbsp; ● Diploma in Computer Engineering</h3>
     </div>
