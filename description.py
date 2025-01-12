1b713f10d32e6bd1e1c63de1d23bbabd8ac9c5d6
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-12 21:54:02 +0530

    Create description.py

---
+++ description.py
@@ -0,0 +1,9 @@
+import streamlit as st
+def description():
+    st.markdown(
+        """
+        <div class="content-container">
+        <h1 class="custom-heading2"> 1. MNIST Digit Recognition:</h1>
+        <h4 class="custom-details">A deep learning project that classifies handwritten digits using the MNIST dataset. Implemented with Convolutional Neural Networks (CNNs) for accurate digit recognition.</h4>
+        """
+    )
