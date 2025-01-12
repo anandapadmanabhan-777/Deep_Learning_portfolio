50dc60dde89509d3073b93ca49e8fcec18fea16c
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-12 15:18:43 +0530

    Update about.py

--- about.py
+++ about.py
@@ -6,18 +6,13 @@ def about():
     <h1 class="custom-heading">About Me</h1>
     <h3 class="custom-lines2"><br>Aspiring AI/ML Engineer 💻🧠 &nbsp; | &nbsp; Python Developer 🐍 &nbsp; | &nbsp; Full Stack Python Developer 🕸️</h3>
     <br>
-    <h1 class="custom-heading2">EDUCATION</h1>
-    <h3 class="custom-lines3"> &nbsp; ● BTech in Infomation Technology<br> &nbsp; ● Diploma in Computer Engineering</h3>
-    
     """,
     unsafe_allow_html=True,
-)
-
+)   
     cv_file_path = "ANANDAPADMANABHAN_CV.pdf"
     # Read the resume file
     with open(cv_file_path, "rb") as file:
         cv_data = file.read()
-
     # Download Button
     st.download_button(
         label="Download CV",
@@ -25,4 +20,17 @@ def about():
         file_name="ANANDAPADMANABHAN_CV.pdf",
         mime="application/pdf",
     )
+
+    st.markdown(
+    """
+    <h1 class="custom-heading2">EDUCATION</h1>
+    <h3 class="custom-lines3"> &nbsp; ● BTech in Infomation Technology<br> &nbsp; ● Diploma in Computer Engineering</h3>
+    
+    """,
+    unsafe_allow_html=True,
+)
+
+    
+
+    
     
