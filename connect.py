ad4ad380865c38f757aac202d973bf439e3d5b6e
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-12 21:14:08 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,7 +1,10 @@
 import streamlit as st 
 def connect():
     st.markdown("""
+    
+    <div class="content-container2>
     <h1 class="custom-heading">Contact Information</h1>
+    </div>
     <br>    
     
     <div style="text-align: center; display: flex; justify-content: center; gap: 20px; margin-top: 40px; margin-bottom: 40px;">
