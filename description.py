4bc060c71a9530f1f99cdf6161ae90620647dbf3
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-12 21:54:55 +0530

    Update description.py

--- description.py
+++ description.py
@@ -5,5 +5,8 @@ def description():
         <div class="content-container">
         <h1 class="custom-heading2"> 1. MNIST Digit Recognition:</h1>
         <h4 class="custom-details">A deep learning project that classifies handwritten digits using the MNIST dataset. Implemented with Convolutional Neural Networks (CNNs) for accurate digit recognition.</h4>
-        """
+        </div>
+
+        """,
+        unsafe_allow_html=True,
     )
