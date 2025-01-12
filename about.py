07e3ba8c0bdef52f2d0334f0a98a07b4e47b11ac
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-12 16:04:38 +0530

    Update about.py

--- about.py
+++ about.py
@@ -26,7 +26,8 @@ def about():
         <h1 class="custom-heading2">TECHNICAL SKILLS</h1>
         <h3 class="custom-details">● Programming Languages:</h3>
         <h3 class="custom-details">&nbsp; &nbsp; Python | SQL</h3>
-        <h3 class="custom-details">● Techniques: <br> &nbsp; &nbsp; Data Preprocessing | Data Augmentation | Model Development | Testing | Analysis | Optimization | UI Development | Image Processing | Pattern Recognition</h3>
+        <h3 class="custom-details">● Techniques: </h3>
+        <h3 class="custom-details">&nbsp; &nbsp; Data Preprocessing | Data Augmentation | Model Development | Testing | Analysis | Optimization | UI Development | Image Processing | Pattern Recognition</h3>
         <h3 class="custom-details">● Machine Learning and Deep Learning Libraries and Frameworks: <br> &nbsp; &nbsp; NumPy | Pandas | Scikit-learn (sklearn) | Matplotlib | Seaborn | Plotly | OpenCV | TensorFlow | Keras | PyTorch</h3>
         <h3 class="custom-details">● Development and Deployment Tools: <br> &nbsp; &nbsp; Docker | Git | GitHub | VS Code | Streamlit | GCP | AWS | CI/CD | Tableau</h3>
         """,
