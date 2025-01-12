67b29f5ee7f6c848346e99acc9f3cef9f9c76d76
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-12 21:26:09 +0530

    Update home.py

--- home.py
+++ home.py
@@ -3,8 +3,8 @@ import streamlit as st
 def home():
     st.markdown(
     """
-    <div class="content-container">
-        <h1 class="custom-heading3">Welcome to My AI & ML Project Portfolio..!!</h1>
+    <div class="content-container3">
+        <h1 class="custom-heading">Welcome to My AI & ML Project Portfolio..!!</h1>
     </div>
     <br>
     <h1 class= "name-heading">Hello, I'm<br> Anandapadmanabhan..!!</h1>
