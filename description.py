b41785effa41b1b4158cf7a6a128e6a81c58be2e
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-13 12:44:44 +0530

    Update description.py

--- description.py
+++ description.py
@@ -14,7 +14,7 @@ def description():
         <h5 class="custom-details">A deep learning project that classifies handwritten digits using the MNIST dataset. Implemented with Convolutional Neural Networks (CNNs) for accurate digit recognition.</h5>
 
         <h3 class="custom-heading3">4. <u>Fashion MNIST Classification</u>:</h3>
-        <h5 class="custom-details">A machine learning project for categorizing Zalando’s fashion items (shirts, shoes, bags) into predefined classes using neural networks.</h5>
+        <h5 class="custom-details">A machine learning project for categorizing Zalando’s fashion items (shirts, shoes, bags, etc) into predefined classes using neural networks.</h5>
 
         <h3 class="custom-heading3">5. <u>Temperature Prediction</u>:</h3>
         <h5 class="custom-details">A time-series forecasting project utilizing a CNN-RNN hybrid model to predict future temperature trends based on historical weather data, enabling accurate environmental analysis and planning.</h5>
