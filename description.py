0e56dce9c894f72893d54428140d49884090bcdc
ANANDAPADMANABHAN S <anandapadmanabhan777@gmail.com>
2025-01-27 14:15:21 +0530

    Update description.py

--- description.py
+++ description.py
@@ -19,6 +19,8 @@ def description():
         <h3 class="custom-heading3">5. <u>Temperature Prediction</u>:</h3>
         <h5 class="custom-details">A time-series forecasting project utilizing a CNN-RNN hybrid model to predict future temperature trends based on historical weather data, enabling accurate environmental analysis and planning.</h5>
 
+        <h3 class="custom-heading3">5. <u>CIFAR-10 Classification</u>:</h3>
+        <h5 class="custom-details">A machine learning project for classifying 60,000 32x32 color images into 10 predefined classes (airplane, car, bird, etc) using convolutional neural networks (CNNs).</h5>
 
         </div>
 
